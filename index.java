2ec795bd947ab45e43de109b4e697f841dd9511d
NaveenWebDev <ns222280@gmail.com>
2023-06-20 15:47:49 +0530

    change hiffen to star

--- index.java
+++ index.java
@@ -7,7 +7,7 @@ public class index{
 
         for(int i = 1; i<=num;i++){
             for(int j = 1; j<=i;j++){
-                System.out.print("- ");
+                System.out.print("* ");
             }
             System.out.println();
         }
