5ec9d2585d9fa59c075ca4e50051a25025a98a86
naveenSharma <ns222280@gmail.com>
2023-06-20 15:04:14 +0530

    index.java

---
+++ index.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class index{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a number");
+        int num = sc.nextInt();
+
+        for(int i = 1; i<=num;i++){
+            for(int j = 1; j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
