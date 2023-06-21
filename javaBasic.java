1c85a92d1f3cd645c48a4cfd9977c977c2db303a
NaveenWebDev <ns222280@gmail.com>
2023-06-21 11:18:38 +0530

    sum of two numbers

--- javaBasic.java
+++ javaBasic.java
@@ -1,7 +1,14 @@
 public class javaBasic {
     public static void main(String args[]) {
-        int numberOne = 5;
-        numberOne = 50;
-        System.out.println(numberOne);
-    }
+        // int numberOne = 5;
+        // numberOne = 21;
+        // String userName = "Naveen Sharma";
+        // System.out.println(userName + " " +  numberOne);
+
+        int numOne = 50;
+        int numTwo = 100;
+        int sum = numOne + numTwo;
+        System.out.println(sum);
+
+  }
 }
