17bf4a60da66ebb08486f64332b12777a39408e1
NaveenWebDev <ns222280@gmail.com>
2023-06-21 10:43:48 +0530

    add variable in java

--- javaBasic.java
+++ javaBasic.java
@@ -1,5 +1,7 @@
 public class javaBasic {
     public static void main(String args[]) {
-        
+        int numberOne = 5;
+        numberOne = 50;
+        System.out.println(numberOne);
     }
 }
