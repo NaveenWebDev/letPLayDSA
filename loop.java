90c2b27d3bf0eabc77ff115eb3e2f6f1af9529c8
NaveenWebDev <ns222280@gmail.com>
2023-06-27 15:00:53 +0530

    after learned loops and reverse number

--- loop.java
+++ loop.java
@@ -2,12 +2,29 @@ import java.util.*;
 public class loop{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("enter a number");
-        int num = sc.nextInt();
-        int sum = 0;
-        for (int i = 1; i <= num ; i++) {
-            sum+=i;   
+        // System.out.print("enter a number");
+        // int num = sc.nextInt();
+        // int sum = 0;
+        // for (int i = 1; i <= num ; i++) {
+        //     sum+=i;   
+        // }
+        // System.out.println(sum);
+
+        // for (int i = 963; i > 0; i /=10 ) {
+        //     int lastNumber = i % 10;  // is line se last number milta hai or (i/=10 isse last number remove ho jata hai);
+        //     System.out.print(lastNumber);
+        // }
+
+        int revNum = 0;
+        for (int i = 963; i > 0; i/=10) {
+            int lastNumber = i % 10;
+            revNum = (revNum * 10) + lastNumber;
         }
-        System.out.println(sum);
+        System.out.println(revNum);
+
+
+
+
+
     }
 }
