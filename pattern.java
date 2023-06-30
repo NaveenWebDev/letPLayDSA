3826c97a09a651e84cf267104e3244b70a30bfe7
NaveenWebDev <ns222280@gmail.com>
2023-06-30 13:24:41 +0530

    INVERTED HALF-PYRAMID with Numbers pattern

--- pattern.java
+++ pattern.java
@@ -48,19 +48,30 @@ public class pattern{
         // }
         // ========================INVERTED & ROTATED HALF-PYRAMID pattern===============================
 
-        int n = 6;
+        // int n = 6;
+        // for (int i = 1; i <=n ; i++) {
+        //     for (int j = 1; j <=n-i+1; j++) {
+        //         System.out.print(" ");
+        //     }
+            
+        //     for (int index = 1; index <=i; index++) {
+        //         System.out.print("*");
+        //     }
+
+        //     System.out.println();
+        // }
+        // ========================INVERTED HALF-PYRAMID with Numbers pattern===============================
+
+        int n = 5;
         for (int i = 1; i <=n ; i++) {
             for (int j = 1; j <=n-i+1; j++) {
-                System.out.print(" ");
+                System.out.print(j);
             }
-            
-            for (int index = 1; index <=i; index++) {
-                System.out.print("*");
-            }
+
 
             System.out.println();
         }
-
+        
 
 
 
