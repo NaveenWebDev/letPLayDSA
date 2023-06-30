9ddb8c4586e3f7f940426fe8e1a1630bfc5d5923
NaveenWebDev <ns222280@gmail.com>
2023-06-30 13:00:45 +0530

    character pattern

--- pattern.java
+++ pattern.java
@@ -14,10 +14,21 @@ public class pattern{
         //     }
         //     System.out.println();
         // }
-        int n = 7;
+        // ========================half pyramid pattern===============================
+        // int n = 4;
+        // for (int i = 1; i<=n; i++) {
+        //     for (int j = 1; j<=i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+        // ========================chracter pattern===============================
+        int n = 4;
+        char ch= 'A';
         for (int i = 1; i<=n; i++) {
             for (int j = 1; j<=i; j++) {
-                System.out.print(j);
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
