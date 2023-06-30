52cf3466959136c3b66099552e37376817272abc
NaveenWebDev <ns222280@gmail.com>
2023-06-30 20:42:48 +0530

    learning functoin

---
+++ function.java
@@ -0,0 +1,13 @@
+public class function{
+    public static int hello(){
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hello());
+        
+    }
+}
