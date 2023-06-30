7810802fd6af3f4908bf953fc4df9818edf3dcde
NaveenWebDev <ns222280@gmail.com>
2023-06-30 12:48:35 +0530

    half pyramid pattern

--- pattern.java
+++ pattern.java
@@ -7,10 +7,17 @@ public class pattern{
         //     }
         //     System.out.println();
         // }
+        // int n = 7;
+        // for (int i = 1; i<=n; i++) {
+        //     for (int j = 1; j<=n-i+1; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
         int n = 7;
         for (int i = 1; i<=n; i++) {
-            for (int j = 1; j<=n-i+1; j++) {
-                System.out.print("*");
+            for (int j = 1; j<=i; j++) {
+                System.out.print(j);
             }
             System.out.println();
         }
