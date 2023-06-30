91f27fd27c2a727cf23fa244c2c107db21cfad4f
NaveenWebDev <ns222280@gmail.com>
2023-06-30 13:18:00 +0530

    hallo ractangle pattern

--- pattern.java
+++ pattern.java
@@ -23,18 +23,29 @@ public class pattern{
         //     System.out.println();
         // }
         // ========================chracter pattern===============================
+        // int n = 4;
+        // char ch= 'A';
+        // for (int i = 1; i<=n; i++) {
+        //     for (int j = 1; j<=i; j++) {
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+        // ========================hallo ractangle pattern===============================
+
         int n = 4;
-        char ch= 'A';
-        for (int i = 1; i<=n; i++) {
-            for (int j = 1; j<=i; j++) {
-                System.out.print(ch);
-                ch++;
+        for (int i = 1; i <=n ; i++) {
+            
+            for (int j = 1; j <=5; j++) {
+                if(j == 1 || j == 5 || i == 1 || i == 4){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
-
-
-
 
 
     }
