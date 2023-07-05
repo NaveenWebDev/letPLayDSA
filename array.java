b6d60a765541a6690a33ac4412f1105d4d018192
NaveenWebDev <ns222280@gmail.com>
2023-07-05 21:18:53 +0530

    arrayPractice

---
+++ array.java
@@ -0,0 +1,10 @@
+public class array {
+    public static void main(String[] args) {
+        System.out.println("hello Array");
+
+        int number[] = {1,2,3,4,5};
+
+        String StudentName[] = {"Naveen","specialist","webDeveloper"};
+        System.out.println(StudentName[0]);
+    }
+}
