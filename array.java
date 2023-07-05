b400b10ce8c37951cde0b59a6976151f8478718c
NaveenWebDev <ns222280@gmail.com>
2023-07-05 21:30:19 +0530

    array size

--- array.java
+++ array.java
@@ -1,10 +1,22 @@
+import java.util.*;
+
 public class array {
     public static void main(String[] args) {
-        System.out.println("hello Array");
+        // System.out.println("hello Array");
 
-        int number[] = {1,2,3,4,5};
+        // int number[] = {1,2,3,4,5};
 
-        String StudentName[] = {"Naveen","specialist","webDeveloper"};
-        System.out.println(StudentName[0]);
+        // String StudentName[] = {"Naveen","specialist","webDeveloper"};
+        // System.out.println(StudentName[0]);
+
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("enter marks");
+            int marksnumber = marks[i] = sc.nextInt();
+            System.out.println("marks is" + marksnumber);
+        }
+
     }
 }
