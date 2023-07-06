b4a4e3705695fcb5b3dfc28f7ab1d96248091754
NaveenWebDev <ns222280@gmail.com>
2023-07-06 20:54:17 +0530

    update array value by passing as an argumnet

--- array.java
+++ array.java
@@ -1,7 +1,23 @@
 import java.util.*;
 
 public class array {
+    public static void updateArry(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = arr[i] + 1;
+        }
+    }
     public static void main(String[] args) {
+
+        int number[] = {97, 98, 99};
+        updateArry(number);
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
+        }
+
+
+
+
+
         // System.out.println("hello Array");
 
         // int number[] = {1,2,3,4,5};
@@ -9,14 +25,15 @@ public class array {
         // String StudentName[] = {"Naveen","specialist","webDeveloper"};
         // System.out.println(StudentName[0]);
 
-        int marks[] = new int[100];
-        Scanner sc = new Scanner(System.in);
+        // int marks[] = new int[100];
+        // Scanner sc = new Scanner(System.in);
 
-        for (int i = 0; i <= 2; i++) {
-            System.out.println("enter marks");
-            int marksnumber = marks[i] = sc.nextInt();
-            System.out.println("marks is" + marksnumber);
-        }
+        // for (int i = 0; i <= 2; i++) {
+        //     System.out.println("enter marks");
+        //     int marksnumber = marks[i] = sc.nextInt();
+        //     System.out.println("marks is" + marksnumber);
+        // }
+
 
     }
 }
