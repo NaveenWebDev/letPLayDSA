cb43b081245e8b8a4ef6a84352e51f03cb7b849b
NaveenWebDev <ns222280@gmail.com>
2023-07-10 12:38:50 +0530

    binary to decimal number code

--- array.java
+++ array.java
@@ -75,11 +75,23 @@ public class array {
         }
         System.out.println("Total subarray is : " + totalSubArray);
     }
+    public static void binToDec(int binNum){
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
 
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum = decNum + (lastDigit * (int)Math.pow(2,pow));
+            pow++;
+            binNum = binNum / 10;
+        }
+        System.out.println("decimal of " + myNum + " = " + decNum);
+    }
     public static void main(String[] args) {
-
-        int number[] = {2, 4, 6, 8, 10};
-        printSubArray(number);
+        binToDec(1000);
+        // int number[] = {2, 4, 6, 8, 10};
+        // printSubArray(number);
         
         
         
