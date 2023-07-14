7df75bce517151d8727f0a7e3fa4a16639210a5d
NaveenWebDev <ns222280@gmail.com>
2023-07-14 21:53:18 +0530

    hallow rhombus pattern solved

--- pattern.java
+++ pattern.java
@@ -98,32 +98,48 @@ public class pattern {
 
         // ========================================Butterfly pattern===============================
 
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            for (int k = 1; k <= 2*(n-i); k++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     for (int k = 1; k <= 2*(n-i); k++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
-        for (int i = n; i>=1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+        // for (int i = n; i>=1; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     for (int k = 1; k <= 2*(n-i); k++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+            // ===================================hallow rhombus==============================
+            int n = 5;
+            for (int i = 1; i <=n; i++) {
+                for (int j = 0; j <=n-i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <=n; j++) {
+                    if(i == 1 || i == 5 || j == 1 || j == 5){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
             }
-            for (int k = 1; k <= 2*(n-i); k++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
 
     }
 }
