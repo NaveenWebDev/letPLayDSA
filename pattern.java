2cf75231c657daad953cb2c47eb234d4b264ffa4
NaveenWebDev <ns222280@gmail.com>
2023-07-14 21:43:55 +0530

    butterfly pattern solved

--- pattern.java
+++ pattern.java
@@ -82,17 +82,46 @@ public class pattern {
         // System.out.println();
         // }
         // ========================Binary Triangle pattern===============================
-        int n = 10;
-        int number = 1;
+        // int n = 10;
+        // int number = 1;
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         if ((i + j) % 2 == 1) {
+        //             System.out.print("0");
+        //         } else {
+        //             System.out.print("1");
+        //         }
+        //     }
+
+        //     System.out.println();
+        // }
+
+        // ========================================Butterfly pattern===============================
+
+        int n = 5;
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                if ((i + j) % 2 == 1) {
-                    System.out.print("0");
-                } else {
-                    System.out.print("1");
-                }
+                System.out.print("*");
             }
+            for (int k = 1; k <= 2*(n-i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+        for (int i = n; i>=1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 1; k <= 2*(n-i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
 
