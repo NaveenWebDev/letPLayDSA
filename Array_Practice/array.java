35e9c9c68901210cffd966509038adf1a5e1d622
NaveenWebDev <ns222280@gmail.com>
2023-08-10 10:07:14 -0700

    BubbleShortAlgorithm

--- Array_Practice/array.java
+++ Array_Practice/array.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 public class array {
 
-    public static void subarray(int arr[]){
+    // public static void subarray(int arr[]){
 
-        int[] arrValue = new int[15];
-        int incrase= 0;
+    //     int[] arrValue = new int[15];
+    //     int incrase= 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                int subarraySum = 0;
-                for (int j2 = i; j2 <= j; j2++) {
-                    subarraySum += arr[j2];
-                }
-                arrValue[incrase] = subarraySum;
-                incrase++;
-                System.out.print(subarraySum + " ");
-            }
-        }
+    //     for (int i = 0; i < arr.length; i++) {
+    //         for (int j = i; j < arr.length; j++) {
+    //             int subarraySum = 0;
+    //             for (int j2 = i; j2 <= j; j2++) {
+    //                 subarraySum += arr[j2];
+    //             }
+    //             arrValue[incrase] = subarraySum;
+    //             incrase++;
+    //             System.out.print(subarraySum + " ");
+    //         }
+    //     }
 
-        System.out.println(Arrays.toString(arrValue));
+    //     System.out.println(Arrays.toString(arrValue));
         
-    }
-   
+        
+    // }
+
+    // public static void Subarray(arr[]){}
     
     public static void main(String[] args) {
 
         int[] number = {2, 4, 6, 8, 10};
 
-        subarray(number);
+        // subarray(number);
 
 
     }
