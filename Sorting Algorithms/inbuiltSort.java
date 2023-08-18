fcb1dc77f8df4008ecb19535d9ea7a3c5f4514b2
NaveenWebDev <ns222280@gmail.com>
2023-08-18 08:35:22 -0700

    array reverse  with indexing

--- Sorting Algorithms/inbuiltSort.java
+++ Sorting Algorithms/inbuiltSort.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 public class inbuiltSort {
     public static void main(String[] args) {
-        Integer number[] = {5, 4, 1, 6, 2, 3}; // collections reverse order me int to capital Interger krna hoga 
+        Integer number[] = {5, 9, 4, 1, 6, 2, 3}; // collections reverse order me int to capital Interger krna hoga 
         // Arrays.sort(number);
         // Arrays.sort(number,1,5);
-        Arrays.sort(number, Collections.reverseOrder());
+        // Arrays.sort(number, Collections.reverseOrder());
+        Arrays.sort(number, 0, 3 , Collections.reverseOrder());
 
         for (int i = 0; i < number.length; i++) {
             System.out.println(number[i]);
