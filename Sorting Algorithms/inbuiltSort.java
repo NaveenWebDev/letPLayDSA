8b8634a9454c1bc0d16557746488d81612280807
NaveenWebDev <ns222280@gmail.com>
2023-08-18 08:33:17 -0700

    arrayReverse With inbuilt function

--- Sorting Algorithms/inbuiltSort.java
+++ Sorting Algorithms/inbuiltSort.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class inbuiltSort {
     public static void main(String[] args) {
-        int number[] = {5, 4, 1, 6, 2, 3};
+        Integer number[] = {5, 4, 1, 6, 2, 3}; // collections reverse order me int to capital Interger krna hoga 
         // Arrays.sort(number);
-        Arrays.sort(number,1,5);
+        // Arrays.sort(number,1,5);
+        Arrays.sort(number, Collections.reverseOrder());
+
         for (int i = 0; i < number.length; i++) {
             System.out.println(number[i]);
         }
