45474c61f572409e15c7f17648f520bf07f1955b
NaveenWebDev <ns222280@gmail.com>
2023-08-18 08:27:19 -0700

    shorting with indexing only

--- Sorting Algorithms/inbuiltSort.java
+++ Sorting Algorithms/inbuiltSort.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class inbuiltSort {
     public static void main(String[] args) {
         int number[] = {5, 4, 1, 6, 2, 3};
-        Arrays.sort(number);
+        // Arrays.sort(number);
+        Arrays.sort(number,1,5);
         for (int i = 0; i < number.length; i++) {
             System.out.println(number[i]);
         }
